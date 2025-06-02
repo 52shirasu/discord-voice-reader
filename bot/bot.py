e455cb22e63c1fe9c96b125be412fc505ddb1025
52shirasu <52shirasu@gmail.com>
2025-06-02 21:15:13 +0900

    #イベントの追加

--- bot/bot.py
+++ bot/bot.py
@@ -4,6 +4,7 @@ from bot.commands.bye import bye
 from bot.commands.setchannel import setchannel
 from bot.events.on_ready import on_ready
 from bot.events.on_message import on_message
+from bot.events.on_voice_state_update import on_voice_state_update
 
 def run_bot():
     if not TOKEN:
